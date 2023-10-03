99335b9806def99e2670e30d364876e4e72fb563
Oualid Fadhlaoui <chahdfadhlaoui87@gmail.com>
2023-10-03 13:48:58 +0200

    mes produits

---
+++ src/app/add-produits/add-produits.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-produits',
+  templateUrl: './add-produits.component.html',
+})
+export class AddProduitsComponent {
+
+}
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+})
+export class ProduitsComponent  implements OnInit {
+  produits : string[];
+
+  constructor() { 
+    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+  }
+ngOnInit(): void {
+
+}
+
+}
